2d638e17877836677e455509d0292d15bff7eccc
MohamadSheikhAlshabab <alshabab.moh@gmail.com>
2023-10-09 18:50:12 +0300

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Please read the README.md to understand what to do. Happy Coding !
+const fetchUserData = async () =>{
+    try{
+        const arrOfRes = [];
+        const usersData = await fetch("https://jsonplaceholder.typicode.com/users");
+        const data = await usersData.json();
+        const result = await data.map( user => {
+            const {name, username, email, phone, website} = user;
+            const address = user.address.street
+            const company = user.company.name;
+            arrOfRes.push(name,username, email, address, phone, website, company);
+        })
+        console.table(arrOfRes);
+
+        const arrOfBizEmail = [];
+        const filterData = data.filter( user => {
+            const filteredEmail = user.email.split('.');
+            filteredEmail.includes('biz') ? arrOfBizEmail.push(user.email) : null;
+        })
+        console.table(arrOfBizEmail);
+
+        const arrOfsortedUser = [];
+        const sortedUser = data.map( user => arrOfsortedUser.push(user.name));
+
+        console.log(arrOfsortedUser.sort())
+
+    }catch (err){
+        console.error(err);
+    }
+}
+
+fetchUserData();
